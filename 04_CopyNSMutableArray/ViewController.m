b78f8e48a7712b35f250d309ba7ba0b2aa412c6c
Yuen-rMBP <rxg9527@sina.cn>
2016-03-24 23:35:21 +0800

    2. 该属性使用了同步锁，会在创建时生成一些额外的代码用于帮助编写多线程程序，这会带来性能问题，通过声明 nonatomic 可以节省这些虽然很小但是不必要额外开销。

--- 04_CopyNSMutableArray/ViewController.m
+++ 04_CopyNSMutableArray/ViewController.m
@@ -23,6 +23,12 @@
     NSMutableArray *array = @[@1, @2, @3].mutableCopy;
     self.mutableArray = array;
     [self.mutableArray addObject:@4];
+    
+    /*
+     atomic
+     并不要求属性必须是“原子的”，因为这并不能保证“线程安全” (thread safety)
+     一个线程在连续多次读取某属性值的过程中有别的线程在同时改写该值，那么即便将属性声明为 atomic，也还是会读到不同的属性值。
+     */
 }
 
 - (void)didReceiveMemoryWarning {
