078ae92353cdfb68918ab3114073b9af0706300b
Yuen-rMBP <rxg9527@sina.cn>
2016-03-25 23:34:02 +0800

    0325 调用resolveInstanceMethod:方法 (或 resolveClassMethod:)。允许用户在此时为该 Class 动态添加实现。如果有实现了，则调用并返回YES，那么重新开始objc_msgSend流程。这一次对象会响应这个选择器，一般是因为它已经调用过class_addMethod。如果仍没实现，继续下面的动作。

--- 25__objc_msgForward_demo/Son.m
+++ 25__objc_msgForward_demo/Son.m
@@ -18,4 +18,14 @@
     return self;
 }
 
+/**
+ *  调用resolveInstanceMethod:方法 (或 resolveClassMethod:)。
+    允许用户在此时为该 Class 动态添加实现。如果有实现了，则调用并返回YES，那么重新开始objc_msgSend流程。
+    这一次对象会响应这个选择器，一般是因为它已经调用过class_addMethod。
+    如果仍没实现，继续下面的动作。
+ */
++ (BOOL)resolveInstanceMethod:(SEL)sel {
+    return YES;
+}
+
 @end
